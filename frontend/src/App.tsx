68804401efd5b99983066b0c2e63c3ea14f1f0a6
Richard Soares <richardbrunnosfc@hotmail.com>
2022-09-09 10:54:20 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import NotificationButton from "./components/notificationButton"
+import SalesCard from "./components/SalesCard"
+import Header from "./components/Header"
 
 function App() {
   return (
   <>
-  <h1> olá, mundo! </h1>
-  <NotificationButton />
+  <Header />
+  <main>
+      <section id="sales">
+        <div className="dsmeta-container">
+        <SalesCard />
+        </div>
+      </section>
+  </main>
+
   </>
 )
 }
